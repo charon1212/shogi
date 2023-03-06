3cb533c56a49cae4aefd9da7243687cf7c2ad427
charon1212 <charons2turquoise@gmail.com>
2023-03-06 14:14:26 +0900

    refactoring

--- solve-problem/src/components/useEditShogiBoard.tsx
+++ solve-problem/src/components/useEditShogiBoard.tsx
@@ -1,78 +1,56 @@
 import { useState } from 'react';
-import { createInitBoard } from '../domain/createInitBoard';
 import { ShogiBoard, ShogiCell, ShogiKoma } from '../domain/shogi';
+import { createShogiBoardAllMochigoma, createShogiBoardInit, createShogiBoardTsume } from '../domain/shogiBoardTemplate';
 import { ShogiMasu } from '../domain/shogiZahyo';
 import { deepCopy } from '../util/deepCopy';
-import { ShogiBoardView } from './ShogiBoardView';
+import { SelectableShogiBoardView } from './SelectableShogiBoardView';
 
-const color = 'yellow';
-const init = createInitBoard();
-const arr9 = [...Array(9)];
+const shogiBoardInit = createShogiBoardInit();
+const shogiBoardAllMochigoma: ShogiBoard = createShogiBoardAllMochigoma();
+const shogiBoardTsume: ShogiBoard = createShogiBoardTsume();
+
+const changeCellOnDoubleClick = (cell: ShogiCell): ShogiCell => {
+  if (!cell) return cell;
+  else if (cell.koma === 'g' || cell.koma === 'k') return { ...cell, sente: !cell.sente };
+  else return { ...cell, nari: !cell.nari, sente: cell.nari ? !cell.sente : cell.sente };
+};
 
 export const useEditShogiBoard = () => {
-  /** 手番 */
-  const [sente, setSente] = useState(true);
-
-  /** 持ち駒 */
-  const [mochigomaSente, setMochigomaSente] = useState(init.mochigoma.sente);
-  const [mochigomaGote, setMochigomaGote] = useState(init.mochigoma.gote);
-  const mochigomaCountUp = (koma: ShogiKoma, sente: boolean, delta: number) => {
-    if (sente) setMochigomaSente({ ...mochigomaSente, [koma]: mochigomaSente[koma] + delta });
-    else setMochigomaGote({ ...mochigomaGote, [koma]: mochigomaGote[koma] + delta });
-  };
-
-  /** 盤面 */
-  const [board, setBoard] = useState<ShogiCell[][]>(init.board);
-  const updateBoard = (updateSet: { masu: ShogiMasu; cell: ShogiCell }[]) => {
-    const newBoard = deepCopy(board);
-    updateSet.forEach(({ masu, cell }) => (newBoard[masu.s - 1][masu.d - 1] = cell));
-    setBoard(newBoard);
-  };
-
-  /** 選択 */
-  const [selectMasu, setSelectMasu] = useState<ShogiMasu | undefined>(undefined);
-  const [selectMochigoma, setSelectMochigoma] = useState<{ sente: boolean; koma: ShogiKoma } | undefined>(undefined);
-  const clearSelection = () => {
-    setSelectMasu(undefined);
-    setSelectMochigoma(undefined);
-  };
-
-  /** 状態から決定される盤面情報 */
-  const shogiBoard: ShogiBoard = {
-    sente,
-    mochigoma: { sente: mochigomaSente, gote: mochigomaGote },
+  const [shogiBoard, setShogiBoard] = useState<ShogiBoard>(deepCopy(shogiBoardInit));
+  const { sente, mochigoma, board } = shogiBoard;
+  const updateShogiBoard = ({
     board,
+    mochigoma,
+  }: {
+    board: { masu: ShogiMasu; cell: ShogiCell }[];
+    mochigoma: { sente: boolean; koma: ShogiKoma; delta: number }[];
+  }) => {
+    const newShogiBoard = deepCopy(shogiBoard);
+    board.forEach(({ masu, cell }) => {
+      newShogiBoard.board[masu.s - 1][masu.d - 1] = cell;
+    });
+    mochigoma.forEach(({ sente, koma, delta }) => {
+      if (sente) newShogiBoard.mochigoma.sente[koma] += delta;
+      if (!sente) newShogiBoard.mochigoma.gote[koma] += delta;
+    });
+    if (sente !== undefined) newShogiBoard.sente = sente;
+    setShogiBoard(newShogiBoard);
   };
 
   /** ボタン押下時の処理 */
   const onClickBtnInit = () => {
-    const conf = window.confirm('初期盤面に戻します。よろしいですか？');
-    if (!conf) return;
-    setSente(true);
-    setMochigomaSente(init.mochigoma.sente);
-    setMochigomaGote(init.mochigoma.gote);
-    setBoard(init.board);
-    clearSelection();
+    if (!window.confirm('初期盤面に戻します。よろしいですか？')) return;
+    setShogiBoard(deepCopy(shogiBoardInit));
   };
   const onClickBtnMochigoma = () => {
-    const conf = window.confirm('全ての駒をお互いの持ち駒にします。よろしいですか？');
-    if (!conf) return;
-    setSente(true);
-    setMochigomaSente({ p: 9, l: 2, n: 2, s: 2, g: 2, r: 1, b: 1, k: 1 });
-    setMochigomaGote({ p: 9, l: 2, n: 2, s: 2, g: 2, r: 1, b: 1, k: 1 });
-    setBoard(arr9.map(() => arr9.map(() => null)));
-    clearSelection();
+    if (!window.confirm('全ての駒をお互いの持ち駒にします。よろしいですか？')) return;
+    setShogiBoard(deepCopy(shogiBoardAllMochigoma));
   };
   const onClickBtnTsume = () => {
-    const conf = window.confirm('詰将棋用盤面にします。よろしいですか？');
-    if (!conf) return;
-    setSente(true);
-    setMochigomaSente({ p: 0, l: 0, n: 0, s: 0, g: 0, r: 0, b: 0, k: 0 });
-    setMochigomaGote({ p: 18, l: 4, n: 4, s: 4, g: 4, r: 2, b: 2, k: 2 });
-    setBoard(arr9.map(() => arr9.map(() => null)));
-    clearSelection();
+    if (!window.confirm('詰将棋用盤面にします。よろしいですか？')) return;
+    setShogiBoard(deepCopy(shogiBoardTsume));
   };
-  const onClickBtnTeban = () => setSente(!sente);
+  const onClickBtnTeban = () => setShogiBoard({ ...shogiBoard, sente: !sente });
 
   /** UI本体 */
   const ui = (
@@ -87,90 +65,83 @@ export const useEditShogiBoard = () => {
         <button onClick={() => onClickBtnTsume()} style={{ margin: '0 5px 0' }}>
           詰将棋
         </button>
-        <button onClick={() => clearSelection()} style={{ margin: '0 5px 0' }}>
-          選択解除
-        </button>
         <button onClick={() => onClickBtnTeban()} style={{ margin: '0 5px 0' }}>
           手番：{sente ? '先手' : '後手'}
         </button>
       </div>
       <div>
-        <ShogiBoardView
+        <SelectableShogiBoardView
           board={shogiBoard}
           allowMochigomaKing
-          colorBoard={selectMasu ? [{ masu: selectMasu, color }] : []}
-          colorMochigoma={selectMochigoma ? [{ ...selectMochigoma, color }] : []}
-          onClickBoard={(masu) => {
+          onClickBoard={({ select, selectMasu, selectMochigoma, clearSelection, clickMasu }) => {
+            const clickCell = board[clickMasu.s - 1][clickMasu.d - 1];
             if (selectMasu) {
-              if (selectMasu.s === masu.s && selectMasu.d === masu.d) {
-                // マスのダブルクリック
-                const cell = board[masu.s - 1][masu.d - 1];
-                if (cell) {
-                  if (cell.koma === 'g' || cell.koma === 'k') {
-                    // GOLD=金とKING=玉は成りがないので、先後の入れ替えのみ。
-                    updateBoard([{ masu, cell: { ...cell, sente: !cell.sente } }]);
-                  } else {
-                    updateBoard([{ masu, cell: { ...cell, nari: !cell.nari, sente: cell.nari ? !cell.sente : cell.sente } }]);
-                  }
-                }
+              const selectCell = board[selectMasu.s - 1][selectMasu.d - 1];
+              if (!selectCell) return clearSelection();
+              if (selectMasu.s === clickMasu.s && selectMasu.d === clickMasu.d) {
+                // セルのダブルクリック
+                updateShogiBoard({ board: [{ masu: selectMasu, cell: changeCellOnDoubleClick(selectCell) }], mochigoma: [] });
                 clearSelection();
               } else {
-                const beforeCell = board[selectMasu.s - 1][selectMasu.d - 1];
-                if (!beforeCell) return;
-                const afterCell = board[masu.s - 1][masu.d - 1];
-                if (afterCell && afterCell.sente === beforeCell.sente) {
-                } else if (afterCell && afterCell.sente !== beforeCell.sente) {
-                  // 両方のセルの駒の持ち主が異なる場合は、「移動後の場所の駒」を「移動元の場所の駒の持ち主の持ち駒」にしてから、移動する。
-                  mochigomaCountUp(afterCell.koma, beforeCell.sente, +1);
-                  updateBoard([
-                    { masu, cell: board[selectMasu.s - 1][selectMasu.d - 1] }, // 移動後のマスを変更
-                    { masu: selectMasu, cell: null }, // 移動元のマスを削除
-                  ]);
+                if (clickCell) {
+                  updateShogiBoard({
+                    board: [
+                      { masu: clickMasu, cell: selectCell }, // 移動後のマスを、移動元の値に
+                      { masu: selectMasu, cell: null }, // 移動元のマスを削除
+                    ],
+                    mochigoma: [{ koma: clickCell.koma, sente: selectCell.sente, delta: +1 }], // 移動後のマスの駒を、選択元側の持ち駒に追加。
+                  });
                 } else {
-                  updateBoard([
-                    { masu, cell: board[selectMasu.s - 1][selectMasu.d - 1] }, // 移動後のマスを変更
-                    { masu: selectMasu, cell: null }, // 移動元のマスを削除
-                  ]);
+                  updateShogiBoard({
+                    board: [
+                      { masu: clickMasu, cell: selectCell }, // 移動後のマスを、移動元の値に
+                      { masu: selectMasu, cell: null }, // 移動元のマスを削除
+                    ],
+                    mochigoma: [],
+                  });
                 }
                 clearSelection();
               }
             } else if (selectMochigoma) {
-              if (!board[masu.s - 1][masu.d - 1]) {
-                mochigomaCountUp(selectMochigoma.koma, selectMochigoma.sente, -1);
-                updateBoard([{ masu, cell: { ...selectMochigoma, nari: false } }]);
+              if (!clickCell) {
+                updateShogiBoard({
+                  board: [{ masu: clickMasu, cell: { ...selectMochigoma, nari: false } }],
+                  mochigoma: [{ ...selectMochigoma, delta: -1 }],
+                });
               }
               clearSelection();
             } else {
-              if (board[masu.s - 1][masu.d - 1]) {
-                setSelectMasu(masu);
-              }
+              if (board[clickMasu.s - 1][clickMasu.d - 1]) select();
             }
           }}
-          onClickMochigoma={(koma, sente) => {
+          onClickMochigoma={({ select, selectMasu, selectMochigoma, clearSelection, clickMochigoma: { koma, sente } }) => {
             if (selectMasu) {
               const cell = board[selectMasu.s - 1][selectMasu.d - 1];
               if (cell) {
-                mochigomaCountUp(cell.koma, sente, +1);
-                updateBoard([{ masu: selectMasu, cell: null }]);
+                updateShogiBoard({
+                  board: [{ masu: selectMasu, cell: null }], // 選択マスを空にする
+                  mochigoma: [{ koma: cell.koma, sente: sente, delta: +1 }], // 持ち駒に+1
+                });
               }
               clearSelection();
-            } else if (selectMochigoma && selectMochigoma.koma === koma && selectMochigoma.sente === sente) {
-              // 持ち駒のダブルクリック
-              if ((sente && mochigomaSente[koma] > 0) || (!sente && mochigomaGote[koma] > 0)) {
-                mochigomaCountUp(koma, sente, -1);
-                mochigomaCountUp(koma, !sente, +1);
-              }
+            } else if (selectMochigoma && selectMochigoma.sente !== sente) {
+              updateShogiBoard({
+                board: [],
+                mochigoma: [
+                  { sente: sente, koma: selectMochigoma.koma, delta: +1 }, // クリック側を+1
+                  { sente: !sente, koma: selectMochigoma.koma, delta: -1 }, // クリック側と逆側を-1
+                ],
+              });
               clearSelection();
             } else {
-              if ((sente && mochigomaSente[koma] > 0) || (!sente && mochigomaGote[koma] > 0)) {
-                setSelectMochigoma({ sente, koma });
-                setSelectMasu(undefined);
-              }
+              if ((sente && mochigoma.sente[koma] > 0) || (!sente && mochigoma.gote[koma] > 0)) select();
             }
           }}
+          onRightClickBoard={({ clearSelection }) => clearSelection()}
+          onRightClickMochigoma={({ clearSelection }) => clearSelection()}
         />
       </div>
     </>
   );
-  return [ui, shogiBoard] as const;
+  return [ui, shogiBoard, setShogiBoard] as const;
 };
