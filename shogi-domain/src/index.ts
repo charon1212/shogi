038b5bd0f8088e060d42868a9ca678e574362867
charon1212 <charons2turquoise@gmail.com>
2023-03-17 22:59:25 +0900

    add

--- shogi-domain/src/index.ts
+++ shogi-domain/src/index.ts
@@ -4,4 +4,5 @@ export * from './domain/Mochigoma';
 export * from './domain/ShogiBoard';
 export * from './domain/ShogiKoma';
 export * from './domain/ShogiMasu';
+export * from './domain/ShogiMove';
 export * from './domain/ShogiTable';
